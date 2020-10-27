2700e12fb4df8bd96dac39277c27285d8a480098
ValdeirLevelUP <72570501+ValdeirLevelUP@users.noreply.github.com>
2020-10-27 13:33:56 +0100

    Refactore
    Proyecto compilado y refactorado

---
+++ Assets/Scripts/Controller/ActionFire.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionFire : IAction
+{
+    #region PRIVATES
+
+    private PlayerController _playerData;
+
+    private Transform ObjectProjectil;
+
+    private List<Rigidbody> _projeteis;
+
+    #endregion
+
+    public ActionFire(PlayerController playerData)
+    {
+        _playerData = playerData;
+
+        _projeteis = new List<Rigidbody>();
+
+        ObjectProjectil = new GameObject("Projecteis").transform;
+
+        for (int i = 0; i < 30; i++)
+        {
+            Rigidbody projectil = MonoBehaviour.Instantiate(_playerData.Projectil, ObjectProjectil,false);
+
+            projectil.gameObject.SetActive(false);
+
+            _projeteis.Add(projectil);
+            
+        }
+    }
+    public void Execute()
+    { 
+        Vector3 mouseInput = Input.mousePosition;
+        mouseInput.z = 10;
+
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(mouseInput);
+        mousePosition.z = 0;
+
+        Vector3 projectileDelta = mousePosition - _playerData.transform.position;
+
+        GetProjecteis().AddForce(projectileDelta.normalized * _playerData.GameData.ProjectileForce);
+    } 
+
+    private Rigidbody GetProjecteis()
+    {
+        Rigidbody projectil = null;
+        for (int i = 0; i < _projeteis.Count; i++)
+        {
+            if (!_projeteis[i].gameObject.activeInHierarchy)
+            {
+                projectil = _projeteis[i];
+
+                projectil.transform.position = _playerData.transform.position;
+
+                projectil.gameObject.SetActive(true);
+
+                break;
+            } 
+        }
+        if(projectil == null)
+        {
+            projectil = MonoBehaviour.Instantiate(_playerData.Projectil, ObjectProjectil, false);
+
+            projectil.transform.position = _playerData.transform.position;
+
+            _projeteis.Add(projectil);
+        }  
+        return projectil;
+    }
+}
---
+++ Assets/Scripts/Interfaces/IInputMoviment.cs
@@ -0,0 +1,5 @@
+﻿using UnityEngine; 
+public interface IInputMoviment
+{
+    Vector3 Direction { get;}
+}
